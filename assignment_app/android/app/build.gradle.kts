47e9813f65906d9969ca44104ed33920025047c9
(dd) <bezawitagumas2@gmail.com>
2025-11-09 15:28:03 +0000

    fix: adjust build.gradle.kts for proper NDK and compile settings

--- assignment_app/android/app/build.gradle.kts
+++ assignment_app/android/app/build.gradle.kts
@@ -1,15 +1,14 @@
 plugins {
     id("com.android.application")
     id("kotlin-android")
-    // The Flutter Gradle Plugin must be applied after the Android and Kotlin Gradle plugins.
-    id("dev.flutter.flutter-gradle-plugin")
+    id("dev.flutter.flutter-gradle-plugin") // Flutter Gradle plugin
 }
 
 android {
     namespace = "com.example.assignment_app"
     compileSdk = flutter.compileSdkVersion
 
-    // ✅ Set correct NDK version manually (matches your installed version)
+    // ✅ Set correct NDK version
     ndkVersion = "29.0.14206865"
 
     compileOptions {
@@ -22,10 +21,7 @@ android {
     }
 
     defaultConfig {
-        // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
         applicationId = "com.example.assignment_app"
-        // You can update the following values to match your application needs.
-        // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
         targetSdk = flutter.targetSdkVersion
         versionCode = flutter.versionCode
@@ -34,8 +30,6 @@ android {
 
     buildTypes {
         release {
-            // TODO: Add your own signing config for the release build.
-            // Signing with the debug keys for now, so `flutter run --release` works.
             signingConfig = signingConfigs.getByName("debug")
         }
     }
